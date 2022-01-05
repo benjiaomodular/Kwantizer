b8c6f89ff9c53a9f9c66f124577f2e3a5a2e393e
Benjie Jiao <hi@benjie.me>
2022-01-05 23:18:56 +0800

    Added initial files

---
+++ Arduino/Kwantizer/Kwantizer.ino
@@ -0,0 +1,168 @@
+#include <avr/pgmspace.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <MCP4725.h>
+#include <ADS1X15.h>
+#include "definitions.h"
+MCP4725 MCP(0x60);
+ADS1115 ADS(0x48);
+
+
+bool tuning_dac = false;
+bool tuning_adc = false;
+
+uint16_t current_scale_mask = 0;
+uint8_t nb_notes_in_scale = 0;
+
+uint8_t current_root_semitone = 1; // Force refresh at start
+
+// in_scale_cv_mode = 0 ->  1 semitone / 83 mV  (non-constant CV difference to change note in scale)
+//                  = 1 ->  1 note in_scale / 83 mV (constant CV difference to change note in scale
+bool volatile in_scale_cv_mode = true;
+
+uint8_t last_semitone_index = 0;
+long int trigger_out_width = 10;
+bool reset_trigger_out = false;
+long int last_trigger_out_millis = millis();
+
+
+/////////// ROTARY ENCODERS //////////
+
+uint8_t volatile D10D11_state = 0b1111;
+uint8_t volatile D6D7_state = 0b1111;
+
+int8_t volatile new_root = 0;
+int8_t volatile new_scale = 0;
+
+bool volatile rotary_change = false;
+//////////////////////////////////////
+
+uint16_t compute_scale_mask(){
+  uint8_t note_count = 0;
+  uint16_t scale_mask = 0;
+
+  if(digitalRead(NOTE_PIN_C)) { scale_mask = scale_mask | 0b100000000000; note_count++;}
+  if(digitalRead(NOTE_PIN_CS)){ scale_mask = scale_mask | 0b010000000000; note_count++;}
+  if(digitalRead(NOTE_PIN_D)) { scale_mask = scale_mask | 0b001000000000; note_count++;}
+  if(digitalRead(NOTE_PIN_DS)){ scale_mask = scale_mask | 0b000100000000; note_count++;}
+  if(digitalRead(NOTE_PIN_E)) { scale_mask = scale_mask | 0b000010000000; note_count++;}
+  if(digitalRead(NOTE_PIN_F)) { scale_mask = scale_mask | 0b000001000000; note_count++;}
+  if(digitalRead(NOTE_PIN_FS)){ scale_mask = scale_mask | 0b000000100000; note_count++;}
+  if(digitalRead(NOTE_PIN_G)) { scale_mask = scale_mask | 0b000000010000; note_count++;}
+  if(digitalRead(NOTE_PIN_GS)){ scale_mask = scale_mask | 0b000000001000; note_count++;}
+  if(digitalRead(NOTE_PIN_A)) { scale_mask = scale_mask | 0b000000000100; note_count++;}
+  if(digitalRead(NOTE_PIN_AS)){ scale_mask = scale_mask | 0b000000000010; note_count++;}
+  if(digitalRead(NOTE_PIN_B)) { scale_mask = scale_mask | 0b000000000001; note_count++;}
+
+  current_scale_mask = scale_mask;
+  nb_notes_in_scale = note_count;
+  Serial.println(current_scale_mask);
+  Serial.println(note_count);
+  
+  return scale_mask;
+}
+
+void setup() {
+  cli();
+  PCICR |= 0b00000101;
+  PCMSK0 |= 0b00001100;
+  PCMSK2 |= 0b11000000;
+  sei();
+
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(NOTE_PIN_C, INPUT);
+  pinMode(NOTE_PIN_CS, INPUT);
+  pinMode(NOTE_PIN_D, INPUT);
+  pinMode(NOTE_PIN_DS, INPUT);
+  pinMode(NOTE_PIN_E, INPUT);
+  pinMode(NOTE_PIN_F, INPUT);
+  pinMode(NOTE_PIN_FS, INPUT);
+  pinMode(NOTE_PIN_G, INPUT);
+  pinMode(NOTE_PIN_GS, INPUT);
+  pinMode(NOTE_PIN_A, INPUT);
+  pinMode(NOTE_PIN_AS, INPUT);
+  pinMode(NOTE_PIN_B, INPUT);
+
+  compute_scale_mask();
+  
+  Wire.begin();
+  Wire.setClock(400000);
+
+  MCP.begin();
+  ADS.begin();
+  ADS.setGain(0);
+
+}
+
+void loop() {
+
+  uint8_t semitones_above_root_in_scale;
+  uint16_t temp_scale_mask = compute_scale_mask();
+  uint8_t note_in_scale;
+  uint8_t root_semitone;
+  int16_t cv_to_quantize = ADS.readADC(0);
+
+//  Serial.print("CV In: ");
+//  Serial.print(cv_to_quantize);
+//  Serial.print("\t");
+
+  if (reset_trigger_out && millis() - last_trigger_out_millis >= trigger_out_width) {
+//    digitalWrite(TRIGGER_OUT_PIN, LOW);
+    reset_trigger_out = false;
+  }
+
+  nb_notes_in_scale = 12;
+
+  note_in_scale = map(
+    cv_to_quantize,
+    CV_0V_BOUNDARY_INCLUSIVE, CV_ABOVE_5V_BOUNDARY_EXCLUSIVE,
+    0, 5 * nb_notes_in_scale + 1);
+
+  semitones_above_root_in_scale = 0;
+  
+  for (uint8_t semitones_above_root = 0, note = 0; semitones_above_root < MAX_DAC_SEMITONE; semitones_above_root++) {
+
+    if (bitRead(temp_scale_mask, 0)) {
+      semitones_above_root_in_scale = semitones_above_root;
+      note += 1;
+      if (note >= note_in_scale) {
+        break;
+      }
+    }
+
+    temp_scale_mask = rotate12Right(temp_scale_mask, 1);
+
+  }
+
+  uint8_t semitone_index = current_root_semitone + semitones_above_root_in_scale;
+
+  semitone_index = min(semitone_index, MAX_DAC_SEMITONE - 1);
+//  
+//  Serial.print(" Note Out: ");
+//  Serial.println((int16_t)pgm_read_word_near(semitone_cvs_dac + semitone_index));
+
+  MCP.setValue((int16_t)pgm_read_word_near(semitone_cvs_dac + semitone_index));
+
+  
+  if (last_semitone_index != semitone_index) {
+    reset_trigger_out = true;
+    last_semitone_index = semitone_index;
+    last_trigger_out_millis = millis();
+  }
+
+}
+
+///////////// Utilities //////////////
+
+int mod(int x, int m) {
+  return (x % m + m) % m;
+}
+
+uint16_t rotate12Left(uint16_t n, uint16_t d) {
+  return 0xfff & ((n << (d % 12)) | (n >> (12 - (d % 12))));
+}
+
+uint16_t rotate12Right(uint16_t n, uint16_t d) {
+  return 0xfff & ((n >> (d % 12)) | (n << (12 - (d % 12))));
+}
---
+++ Arduino/Kwantizer/definitions.h
@@ -0,0 +1,114 @@
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define NOTE_PIN_C 2
+#define NOTE_PIN_D 3
+#define NOTE_PIN_E 4
+#define NOTE_PIN_F 5
+#define NOTE_PIN_G 6
+#define NOTE_PIN_A 7
+#define NOTE_PIN_B 8
+
+#define NOTE_PIN_CS 9
+#define NOTE_PIN_DS 10
+#define NOTE_PIN_FS 11
+#define NOTE_PIN_GS 12
+#define NOTE_PIN_AS 13
+
+#define MAX_DAC_SEMITONE 61
+const int16_t PROGMEM semitone_cvs_dac[MAX_DAC_SEMITONE] = {
+// 0     83.3  166.6  250   333   416.6  500  583.3  666.6  750   833.3  916.6
+  6,     65,   131,   201,  268,  337,   405,  474,  540,   609,  677,   745,
+  814,   882,  950,  1016, 1084, 1151,  1223, 1290, 1359,  1427, 1496,  1563, 
+  1631, 1699, 1768,  1838, 1904, 1974,  2041, 2110, 2177,  2248, 2315,  2386,
+  2455, 2524, 2592,  2660, 2729, 2798,  2867, 2934, 3004,  3071, 3141,  3206,
+  3278, 3344, 3412,  3480, 3549, 3617,  3685, 3754, 3822,  3892, 3960,  4026,
+  4093
+};
+
+
+
+// 0  83  167  250  333  417  500  583  667  750  833  917
+// Techically the ADC can distinguish amongst 63 semitones, but the DAC can
+// only produce 57 because of the power of my USB port
+
+#define CV_0V_BOUNDARY_INCLUSIVE -32
+#define CV_ABOVE_5V_BOUNDARY_EXCLUSIVE 26832
+#define NB_ADC_BOUNDARIES 62
+
+// ADS1015 maps 6.14 at 32767, but we stop at 5V to stay within standards.
+// These are the inter CVs ADC reading, i.e. we round CVs by placing boundaries
+// at 1/24V left and right of the CV associated with a semitone,
+// in other words if you are within k*83.3mV ± 41.6mV then bin CV into k
+const int16_t PROGMEM inter_semitonecv_to_ADC16read[NB_ADC_BOUNDARIES] = {
+        -240,   208,   656,  1104,  1544,  1984,  2432,  2880,  3328,  3776,  4216,  4656,
+        5096,  5544,  5992,  6432,  6872,  7320,  7776,  8216,  8648,  9088,  9536,  9984,
+       10432, 10880, 11320, 11760, 12208, 12648, 13088, 13536, 13984, 14424, 14864, 15312,
+       15752, 16200, 16656, 17104, 17552, 17992, 18432, 18872, 19312, 19760, 20200, 20648,
+       21096, 21536, 21976, 22416, 22872, 23312, 23752, 24200, 24648, 25096, 25536, 25984, 
+       26432, // 26880,
+       32765
+};
+
+#define NUM_NOTES 12
+#define NUM_SCALES 120
+
+#define KEYBOARD_WIDTH 11
+#define SCALE_BITS 12
+
+#define WKEY_WIDTH 7
+#define WKEY_HEIGHT 12
+
+#define BKEY_WIDTH 5
+#define BKEY_HEIGHT 10
+
+#define KEYBOARD_LAYOUT_WIDTH 56
+#define KEYBOARD_LAYOUT_HEIGHT 16
+
+// Draws the layout smaller or bigger
+#define PROP 2
+#define ROOT_DOT_SIZE 3
+
+
+// [0] = 0 -> white key
+//     = 1 -> black key
+// [1] delta x to next note (white->black or black->white might not be the same
+//                           if one is thinner/wider than the other)
+// [2] heigth of lower left corder
+// [3] width
+// [4] height
+// [5] rounded corner radius
+const int8_t piano_layout[12][6] =
+{
+  {0, 5, 2, 7, 14, 2},    // W
+  {1, 3, 0, 5, 12, 2},    // K
+  {0, 5, 2, 7, 14, 2},    // W
+  {1, 3, 0, 5, 12, 2},   // K
+  {0, 8, 2, 7, 14, 2},   // W
+  {0, 5, 2, 7, 14, 2},   // W
+  {1, 3, 0, 5, 12, 2},   // K
+  {0, 5, 2, 7, 14, 2},   // W
+  {1, 3, 0, 5, 12, 2},   // K
+  {0, 5, 2, 7, 14, 2},   // W
+  {1, 3, 0, 5, 12, 2},   // K
+  {0, 8, 2, 7, 14, 2}    // W
+};
+
+
+const int8_t beatstep_layout[12][6] =
+{
+  {0, 0, 9, 7, 7, 2},    // W
+  {1, 8, 1, 7, 7, 2},    // K
+  {0, 0, 9, 7, 7, 2},    // W
+  {1, 8, 1, 7, 7, 2},   // K
+  {0, 8, 9, 7, 7, 2},   // W
+  {0, 0, 9, 7, 7, 2},   // W
+  {1, 8, 1, 7, 7, 2},   // K
+  {0, 0, 9, 7, 7, 2},   // W
+  {1, 8, 1, 7, 7, 2},   // K
+  {0, 0, 9, 7, 7, 2},   // W
+  {1, 8, 1, 7, 7, 2},   // K
+  {0, 8, 9, 7, 7, 2}    // W
+
+};
+
+const int8_t (*keyboard_layouts[2])[6] = {piano_layout, beatstep_layout};
+const int8_t (*keyboard_layout)[6];
